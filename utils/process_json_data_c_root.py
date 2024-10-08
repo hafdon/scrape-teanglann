268987bd6a2260e8d579e2daa90714cbcbefb89f
hafdon <zweisser@gmail.com>
2024-10-08 02:19:01 -0700

    feat: add teanglann-openai pipeline

--- utils/process_json_data_c_root.py
+++ utils/process_json_data_c_root.py
@@ -1,18 +1,23 @@
+"""
+This script processes the JSON data from the Teanglann scraper and extracts the conditional forms of the root verb.
+"""
+
 import json
 
 with open('output.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
+print()
+
 for entry in data:
     word = entry['url']
-    verbal_noun = entry['item'].get('verbal_noun')
-    past_singular = entry['item'].get('past', {}).get('SINGULAR', [])
-    future = entry['item'].get('future', {}).get('SINGULAR', [])[0].split(' ')[0]
-    # Process or print the data as needed
 
-    present_autonomous = entry['item'].get('present', {}).get('PASSIVE', [])[0].split(' ')[0]
-    imperative_autonomous = entry['item'].get('imper', {}).get('PASSIVE', [])[0].split(' ')[0]
-    past_habitual_autonomous = entry['item'].get('pastConti', {}).get('PASSIVE', [])[0].split(' ')[0]
-    past_habitual_syn_2sg = entry['item'].get('pastConti', {}).get('SINGULAR', [])[3].split(' ')[0]
+    conditional_analytic = entry['item'].get('condi', {}).get('SINGULAR', [])[6].split(' ')[0]
+    conditional_synthetic_1sg = entry['item'].get('condi', {}).get('SINGULAR', [])[0]
+    conditional_synthetic_2sg = entry['item'].get('condi', {}).get('SINGULAR', [])[3]
+    conditional_synthetic_1pl = entry['item'].get('condi', {}).get('PLURAL', [])[0]
+    conditional_synthetic_3pl = entry['item'].get('condi', {}).get('PLURAL', [])[9]
+    conditional_autonomous = entry['item'].get('condi', {}).get('PASSIVE', [])[0]
 
-    print(present_autonomous, imperative_autonomous, past_habitual_autonomous, past_habitual_syn_2sg)
+
+    print(conditional_analytic, conditional_synthetic_1sg, conditional_synthetic_2sg, conditional_synthetic_1pl, conditional_synthetic_3pl, conditional_autonomous)
--- utils/process_json_data_w_root.py
+++ utils/process_json_data_w_root.py
@@ -11,7 +11,8 @@ for entry in data:
     # Process or print the data as needed
 
     present_autonomous = entry['item'].get('present', {}).get('PASSIVE', [])[0].split(' ')[0]
-    imperative_autonomous = entry['item'].get('imper', {}).get('PASSIVE', [])[0].split(' ')[0]
+    # Remove trailing exclamation mark if present
+    imperative_autonomous = entry['item'].get('imper', {}).get('PASSIVE', [])[0].split('!')[0]
     past_habitual_autonomous = entry['item'].get('pastConti', {}).get('PASSIVE', [])[0].split(' ')[0]
     past_habitual_syn_2sg = entry['item'].get('pastConti', {}).get('SINGULAR', [])[3].split(' ')[0]
 
